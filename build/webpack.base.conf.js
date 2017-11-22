279a90d8bc3b08b9fe52e4f5bf9e47eb45ea8f7a
LCN <1455476411@qq.com>
2017-11-22 13:30:24 +0800

    install jquery,vuex

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,8 +1,9 @@
 'use strict'
-const path = require('path')
-const utils = require('./utils')
-const config = require('../config')
-const vueLoaderConfig = require('./vue-loader.conf')
+const path = require('path');
+const utils = require('./utils');
+const config = require('../config');
+const vueLoaderConfig = require('./vue-loader.conf');
+const webpack = require('webpack');
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -64,5 +65,11 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      jQuery: "jquery",
+      $: "jquery"
+    })
+  ],
 }
